6635fb49c89d02555eb2cdab03c01fc9ad0e62ac
desertcolleges <taylor@desertcolleges.org>
2026-09-24 13:53:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = 'https://dfghvbhidpzxjdcczcck.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_IXoPGcfK7o5LbccQvk6g9g_cWbtfrH2';
 
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
